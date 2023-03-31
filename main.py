8c211b85d432cd0f274e932486a5861b76eb6b8c
Dhiraj Birajdar <birajdar257@gmail.com>
2023-03-31 22:10:00 +0530

    bricks drawn, bottom pad is working on key press

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
 from turtle import Turtle, Screen
 
-player_ball = Turtle('circle')
 
 screen = Screen()
+screen.setup(height=800, width=600)
+colors = ['red', 'green', 'blue']
+bricks = []
+
+
+def draw_bricks():
+    global bricks
+    x = -276
+    y = 370
+    for r in range(4):
+        y -= 1 * 22
+        for c in range(13):
+            t = Turtle('square')
+            t.color(colors[c%3])
+            t.speed(0)
+            t.shapesize(1, 2)
+            bricks.append(t)
+            t.goto(x+c*45, y)
+
+
+def move_left():
+    pad.backward(20)
+
+
+def move_right():
+    pad.forward(20)
+
+
+ball = Turtle('circle')
+pad = Turtle('square')
+pad.speed(0)
+pad.shapesize(1, 4)
+pad.goto(0, -380)
+screen.listen()
+screen.onkeypress(key='Left', fun=move_left)
+screen.onkeypress(key='Right', fun=move_right)
+draw_bricks()
+
 screen.exitonclick()
